59988bfa50e0cebb0d47618a803dc62debad88c6
LateMarch <73418661+Latemarch@users.noreply.github.com>
2024-01-28 22:11:09 +0900

    create app and add arc

---
+++ src/app/arc/page.tsx
@@ -0,0 +1,9 @@
+import MapWithArc from "@/components/MapWithArc";
+
+export default function page() {
+  return (
+    <div className="w-full h-full flex ">
+      <MapWithArc />
+    </div>
+  );
+}
---
+++ src/components/MapWithArc.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { LineLayer } from "@deck.gl/layers/typed";
+import DeckGL from "@deck.gl/react/typed";
+import { GeoJsonLayer } from "@deck.gl/layers/typed";
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {
+    sourcePosition: [-122.41669, 37.7853],
+    targetPosition: [-122.41669, 37.781],
+  },
+];
+
+// DeckGL react component
+export default function MapWithArc() {
+  const layers = [new LineLayer({ id: "line-layer", data })];
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+    />
+  );
+}
