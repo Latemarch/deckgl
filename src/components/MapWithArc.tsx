f6ed83e15145aa17b7b312be06a1033acc862944
Latemarch <dolphin0331@naver.com>
2024-01-31 16:21:04 +0900

    get mapbox map

--- src/components/MapWithArc.tsx
+++ src/components/MapWithArc.tsx
@@ -3,21 +3,23 @@ import React from "react";
 import { LineLayer } from "@deck.gl/layers/typed";
 import DeckGL from "@deck.gl/react/typed";
 import { GeoJsonLayer } from "@deck.gl/layers/typed";
+import { Map } from "react-map-gl";
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
+  longitude: 126.9918,
+  latitude: 37.552,
+  zoom: 10,
   pitch: 0,
   bearing: 0,
 };
+const mapBoxKey = process.env.NEXT_PUBLIC_MAPBOX_APIKEY;
 
 // Data to be used by the LineLayer
 const data = [
   {
-    sourcePosition: [-122.41669, 37.7853],
-    targetPosition: [-122.41669, 37.781],
+    sourcePosition: [126.99189297042933, 37.55209372016461],
+    targetPosition: [126.99189297042933, 37.25],
   },
 ];
 
@@ -30,6 +32,17 @@ export default function MapWithArc() {
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
-    />
+    >
+      <Map
+        attributionControl={false}
+        mapStyle="mapbox://styles/mapbox/light-v10"
+        mapboxAccessToken={mapBoxKey}
+      >
+        {/* <NavigationControl position="top-right" />
+        <FullscreenControl position="top-right" />
+        <AttributionControl position="bottom-right" />
+        <ScaleControl position="bottom-right" /> */}
+      </Map>
+    </DeckGL>
   );
 }
